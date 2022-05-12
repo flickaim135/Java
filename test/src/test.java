44f238bfbb3da3832386e6b919a8b8b6d62f6586
flickaim135 <flickaim135@gmail.com>
2022-05-12 19:40:43 +0600

    first java code

---
+++ test/src/test.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class test
+{
+  public static void main(String[] args)
+  {
+    /* Print Output in Java */
+    System.out.print("Hello Java");
+    System.out.println("Hello Java");
+    /* Data types & variables in Java */
+    int x = 10, y = 1540;
+    double z = 155.9148;
+    System.out.println(x + z);
+    System.out.println(x + y);
+    System.out.println(x + y + z);
+    System.out.println(x + y + z);
+    System.out.println(x * y);
+    System.out.println(y / z);
+    System.out.println(z / x);
+
+  }
+}
