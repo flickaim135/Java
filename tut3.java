722ab3787f9ba46673d142c4e48a98e96131cb16
flickaim135 <flickaim135@gmail.com>
2022-06-13 22:40:35 +0600

    some code updated

--- tut3.java
+++ tut3.java
@@ -1,27 +1,7 @@
-import java.util.Scanner;
-
 public class tut3
 {
     public static void main(String[] args)
-    {
-        /* =============== Problem 04 =============== */
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Input a number: ");
-        int a = sc.nextInt();
-        System.out.print("Input last number: ");
-        int b = sc.nextInt();
-        int sum = 0;
-        for(int i = a; i< b; i++)
         {
-            if(i%2 == 0)
-            {
-                sum = sum + i;
-            }
+            System.out.println("Hello World");
         }
-        System.out.print("Sum is: " + sum);
-
-        /* 123 -> 123, 132, 213, 231, 321, 312 
-        
-         */
-    }
 }
